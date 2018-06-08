562a6c97ce42ef73d9d3359db7ee5f3267bbe447
june <kimjy4536@hotmail.com>
2018-06-08 14:45:38 +0900

    create meet up list

--- src/components/MeetList.js
+++ src/components/MeetList.js
@@ -1,27 +1,75 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import MeetUp from './MeetUp'
 
-export default class MeetList extends Component {
-  // const arr= [{},{},{},];
-  render() {
-    return (
-      <div>
-      <MeetUp hostname={'june'}/>
-      <MeetUp />
-      <MeetUp />
-      <MeetUp />
-      <MeetUp />
-      <MeetUp />
-      </div>
-    )
-  }
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
-}  
+const TableHeadCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+  cell: {
+    width: '33%',
+    textAlign: 'center',
+  },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+      textAlign: 'center',
+    },
+  },
+});
+
+function MeetList(props) {
+  const { classes } = props;
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell className={classes.cell}>Title</TableHeadCell>
+            <TableHeadCell className={classes.cell}>Host Name</TableHeadCell>
+            <TableHeadCell className={classes.cell}>Date</TableHeadCell>            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+          <MeetUp title={'title1'} name={'june'} date={'2015-01-12'} />
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+MeetList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MeetList);
--- src/components/MeetUp.js
+++ src/components/MeetUp.js
@@ -1,19 +1,40 @@
-import React,{Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-export default class MeetUp extends Component {
-  render() {
-    console.log(this.props)
-    return (
-      <TextField>{this.props.hostname}</TextField>
-    )
-  }
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
-}  
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = theme => ({
+  cell: {
+    width: '33%',
+    textAlign: 'center',
+  },
+});
+
+function MeetUp(props) {
+  const { classes, title, name, date } = props;
+
+  return (
+    <TableRow>
+      <CustomTableCell className={classes.cell}>{title}</CustomTableCell>
+      <CustomTableCell className={classes.cell}>{name}</CustomTableCell>
+      <CustomTableCell className={classes.cell}>{date}</CustomTableCell>
+    </TableRow>
+  );
+}
+
+MeetUp.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MeetUp);
